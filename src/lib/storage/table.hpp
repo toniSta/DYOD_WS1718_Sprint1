3c4ad3790f60f0d1646f3ad960bee28c7c8a971a
Marcel Jankrift <marcel@jankrift.de>
2017-10-25 20:10:49 +0200

    step 5

--- src/lib/storage/table.hpp
+++ src/lib/storage/table.hpp
@@ -24,8 +24,8 @@ class Table : private Noncopyable {
   // creates a table
   // the parameter specifies the maximum chunk size, i.e., partition size
   // default (0) is an unlimited size. A table holds always at least one chunk
-  explicit Table(const uint32_t chunk_size = 0) { max_chunk_size = chunk_size; };
-  // TODO refactor
+  explicit Table(const uint32_t chunk_size = 0) { max_chunk_size = chunk_size; }
+  // TODO(marcel): refactor
 
   // we need to explicitly set the move constructor to default when
   // we overwrite the copy constructor
