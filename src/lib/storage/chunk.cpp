ff5bc8a6dba940200ee61303395eaa6146d0454a
Marcel Jankrift <marcel@jankrift.de>
2017-10-25 14:01:00 +0200

    Implement step 4

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -17,7 +17,11 @@ namespace opossum {
 void Chunk::add_column(std::shared_ptr<BaseColumn> column) { chunk_columns.push_back(column); }
 
 void Chunk::append(std::vector<AllTypeVariant> values) {
-  // Implementation goes here
+  DebugAssert(values.size() == this->col_count(), "Wrong number of values.");
+
+  for (uint16_t column_id = 0; column_id < values.size(); ++column_id) {
+    this->get_column(ColumnID{column_id})->append(values.at(column_id));
+  }
 }
 
 std::shared_ptr<BaseColumn> Chunk::get_column(ColumnID column_id) const { return chunk_columns.at(column_id); }
@@ -25,7 +29,7 @@ std::shared_ptr<BaseColumn> Chunk::get_column(ColumnID column_id) const { return
 uint16_t Chunk::col_count() const { return chunk_columns.size(); }
 
 uint32_t Chunk::size() const {
-  if (chunk_columns.size() == 0) {
+  if (chunk_columns.empty()) {
     return 0;
   } else {
     return this->get_column(ColumnID{0})->size();
