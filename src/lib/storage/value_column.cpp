74c704b19b9aa8369db3082a52ddce69572d10eb
Marcel Jankrift <marcel@jankrift.de>
2017-10-25 13:16:18 +0200

    Implement step 2

--- src/lib/storage/value_column.cpp
+++ src/lib/storage/value_column.cpp
@@ -16,19 +16,17 @@ namespace opossum {
 template <typename T>
 const AllTypeVariant ValueColumn<T>::operator[](const size_t i) const {
   PerformanceWarning("operator[] used");
-
-  throw std::runtime_error("Implement ValueColumn::operator[]");
+  return column_values.at(i);
 }
 
 template <typename T>
 void ValueColumn<T>::append(const AllTypeVariant& val) {
-  // Implementation goes here
+  column_values.push_back(type_cast<T>(val));
 }
 
 template <typename T>
 size_t ValueColumn<T>::size() const {
-  // Implementation goes here
-  return 0;
+  return column_values.size();
 }
 
 EXPLICITLY_INSTANTIATE_COLUMN_TYPES(ValueColumn);
