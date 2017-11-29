cb95131919a1c7da2e9ec53b09801b764a7299a7
Marcel Jankrift <marcel@jankrift.de>
2017-11-29 11:46:19 +0100

    implement reference column

--- src/lib/storage/reference_column.hpp
+++ src/lib/storage/reference_column.hpp
@@ -22,18 +22,27 @@ class ReferenceColumn : public BaseColumn {
   // creates a reference column
   // the parameters specify the positions and the referenced column
   ReferenceColumn(const std::shared_ptr<const Table> referenced_table, const ColumnID referenced_column_id,
-                  const std::shared_ptr<const PosList> pos);
+                  const std::shared_ptr<const PosList> pos)
+      : _referenced_table(referenced_table), _referenced_column_id(referenced_column_id), _pos(pos) {}
 
-  const AllTypeVariant operator[](const size_t i) const override;
+  const AllTypeVariant operator[](const size_t i) const override {
+    const auto row_id = _pos->at(i);
+    return (*_referenced_table->get_chunk(row_id.chunk_id).get_column(_referenced_column_id))[row_id.chunk_offset];
+  };
 
-  void append(const AllTypeVariant&) override { throw std::logic_error("ReferenceColumn is immutable"); };
+  void append(const AllTypeVariant&) override { throw std::logic_error("ReferenceColumn is immutable"); }
 
-  size_t size() const override;
+  size_t size() const override { return _pos->size(); }
 
-  const std::shared_ptr<const PosList> pos_list() const;
-  const std::shared_ptr<const Table> referenced_table() const;
+  const std::shared_ptr<const PosList> pos_list() const { return _pos; }
+  const std::shared_ptr<const Table> referenced_table() const { return _referenced_table; }
 
   ColumnID referenced_column_id() const;
+
+ protected:
+  const std::shared_ptr<const Table> _referenced_table;
+  const ColumnID _referenced_column_id;
+  const std::shared_ptr<const PosList> _pos;
 };
 
 }  // namespace opossum
